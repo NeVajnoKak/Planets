25b87fdfc75f4b79b7230339af414b33a696c4fa
NeVajnoKak <156511620+NeVajnoKak@users.noreply.github.com>
2024-07-26 22:52:12 +0500

    Initial Commit

---
+++ Planets/PlanetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetsApp.swift
+//  Planets
+//
+//  Created by Erkebulan Massainov on 26.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct PlanetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
